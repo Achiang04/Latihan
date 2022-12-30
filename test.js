46f9e2f9ccd15a6f9668ab43f2d090f23ba3b158
Jecky Hindrawan <jecky@coconutlab.io>
2022-12-30 22:31:44 +0700

    -

--- test.js
+++ test.js
@@ -69,3 +69,7 @@
 // console.log(power(7, 3)); // 343
 // console.log(power(3, 3)); // 27
 // console.log(power(4, 0.5)); // 2
+
+// class Car {}
+// const car = new Car();
+// console.log(typeof car);

8728c44f8f3a9ccfd470e3bb7e9fe4bbd284d677
Jecky Hindrawan <jecky@coconutlab.io>
2022-12-31 16:45:00 +0700

    -

---
+++ Dicoding/BE/nodejs-basic/process-object/index.js
@@ -0,0 +1,20 @@
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for (let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
+
+// to run this file
+// NODE_ENV=development node ./process-object/index.js <Nama Anda>
+// contoh
+// NODE_ENV=development node ./process-object/index.js asep
